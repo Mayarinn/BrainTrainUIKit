d22ec60b2f3ffa24aaaf26d1bb65d0143f863e23
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-22 08:26:03 +0300

    ADD: Set ChangeModeViewController as initial view

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        let initialViewController = Multiply1x1ViewController()
+        let initialViewController = ChangeModeViewController()
         window.rootViewController = initialViewController
         window.makeKeyAndVisible()
         self.window = window
