deccb738d01fc24cda2657c4a09506bf3243ab46
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-22 08:32:48 +0300

    ADD: multiply1x1Button variable

--- ChangeModeViewController.swift
+++ ChangeModeViewController.swift
@@ -11,6 +11,17 @@ class ChangeModeViewController: UIViewController {
     
 //MARK: - variables
     
+    var multiply1x1Button: UIButton = {
+        var multiply1x1Button = UIButton()
+        multiply1x1Button.setTitle("1x1", for: .normal)
+        multiply1x1Button.layer.cornerRadius = 7
+        multiply1x1Button.backgroundColor = .systemPurple
+        multiply1x1Button.setTitleColor(.systemPink, for: .highlighted)
+        multiply1x1Button.translatesAutoresizingMaskIntoConstraints = false
+            
+        return multiply1x1Button
+    }()
+    
 //MARK: - functions
             
     override func viewDidLoad() {
